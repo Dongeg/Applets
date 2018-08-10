a0173fb3c0ba41bf778b1ec1eb56b728fb434980
Dongeg <821220992@qq.com>
2018-08-10 22:53:36 +0800

    完成开发

--- pages/searchPage/searchPage.js
+++ pages/searchPage/searchPage.js
@@ -1,66 +1,50 @@
 // pages/searchPage/searchPage.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    key:'',
+    searchResult:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function () {
   
   },
+  getkey:function(e){
+    this.setData({
+      key: e.detail.value
+    })
+  },
+  back:function(){
+    wx.navigateBack();
+  },
+  // 跳转详情页
+  toDetails: function (e) {
+    var id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '../movieDetails/movieDetails?id=' + id
+    })
+  },
+  getSearchResult: function (city) {
+    var that = this;
+    app.getData({
+      url: '/v2/movie/search',
+      data:{
+        q:that.data.key
+      },
+      callback: function (res) {
+        that.setData({
+          searchResult: res.data.subjects
+        })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
